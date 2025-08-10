713b491870ac66b06a9ef2e0948bb5d46b0eb205
Case Ollila <crollila@gmail.com>
2025-08-10 00:16:56 -0600

    Add explicit ImportMeta/Env types for Vite

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 ﻿/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_PORTAL_PASSWORD?: string;
+  // add more VITE_ vars here as needed
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
